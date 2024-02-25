e9fb2c7c9282db0c927f31645c1099be36f0aa10
surendra Babu C <surendra9703802865@gmail.com>
2024-02-25 22:10:22 +0530

    Prod Setup

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,9 +14,17 @@ pipeline {
         choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick Environment')
 
         string(name: 'component', defaultValue: '', description: 'Provide the Component Name')
+
+        string(name: 'app_version', defaultValue: '', description: 'Provide the Deployment APP Version')
     }
 
     stages {
+
+        stage('Update Parameter Store') {
+            steps {
+                sh 'aws ssm put-parameter --name "${env}.${component}.app_version" --type "String" --value "${app_version}" --overwrite'
+            }
+        }
 
         stage('Ansible') {
             steps {
